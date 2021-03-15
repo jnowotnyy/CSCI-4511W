eb17116762bccc926792366e7fcb453b197f3484
nowot005 <nowot005@umn.edu>
2021-03-15 07:30:57 -0500

    changes to event.java

--- MCTS-Euchre/src/Event.java
+++ MCTS-Euchre/src/Event.java
@@ -1,3 +1,12 @@
 public enum Event {
-  
+  DEAL,
+  CUT, //optional
+  PLAY,
+  TRICKWIN, //an individual trick is won
+  TRUMPCARD, //
+  DEALERWIN, //dealer wins 3 or 4 tricks (1 pt)
+  EUCHERED, //defender wins 3 or 4 tricks (2 pts)
+  DEALERSWEEP, // dealer wins all 5 tricks
+  DEFENDERSWEEP, //defender wins all 5 tricks
+  WIN //first player to score 10 pts
 }
